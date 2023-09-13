c9cf223d64736ba15b25bc70cde6c3a67d3c06cd
LeeBingler <binglerlee@gmail.com>
2023-09-13 17:28:39 +0200

    feat: add style navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,35 @@
+import { RiTwitterXFill, RiDiscordFill } from 'react-icons/ri';
+
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav>
+            <div className='navbar'>
+                <div className='logo'>
+                    <a href='#Home'> Awesomecrypt </a>
+                </div>
+
+                <ul className='listNavbar'>
+                    <li>
+                        <a href='#Home'> Home </a>
+                    </li>
+                    <li>
+                        <a href='#Market'> Market </a>
+                    </li>
+                    <li>
+                        <a href='#ChooseUs'> Choose Us </a>
+                    </li>
+                    <li>
+                        <a href='#Join'> Join </a>
+                    </li>
+                </ul>
+
+                <div>
+                    <RiTwitterXFill />
+                    <RiDiscordFill />
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
