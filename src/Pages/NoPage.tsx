064171b17de7ed5c3caa0a74915cc34a5c79d7e7
LeeBingler <binglerlee@gmail.com>
2023-09-17 23:13:35 +0200

    feat: style nopage

--- src/Pages/NoPage.tsx
+++ src/Pages/NoPage.tsx
@@ -1,7 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import BtcImage from '../assets/images/bitcoin.png';
+
 function NoPage() {
-  return (
-    <div>NoPage</div>
-  )
+    const navigate = useNavigate();
+    return (
+        <section className='nopage'>
+            <div className='nopageTitle'>
+                4 <img src={BtcImage} alt='Bitcoin image that stand for a zero' /> 4
+            </div>
+            <p> There's nothing to see here</p>
+            <button aria-label='Go Home button' onClick={() => navigate('/')}>
+                Go Home
+            </button>
+        </section>
+    );
 }
 
-export default NoPage
+export default NoPage;
