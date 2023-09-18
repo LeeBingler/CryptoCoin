05de2301a5234f33bee17c410db5f7fbbba78c8f
LeeBingler <binglerlee@gmail.com>
2023-09-18 21:02:18 +0200

    feat: add joinUs section + style

--- src/Components/JoinUs/JoinUs.tsx
+++ src/Components/JoinUs/JoinUs.tsx
@@ -1,7 +1,17 @@
 function JoinUs() {
-  return (
-    <div>JoinUs</div>
-  )
+    return (
+        <section className='joinUsContainer'>
+            <article className='joinUs'>
+                <div>
+                    <h1>
+                        Join Us Via <br /> <span> Discord </span>
+                    </h1>
+                    <p> We are the most reliable exchange of Cryptocurrencies </p>
+                </div>
+                <button> Join the adventure </button>
+            </article>
+        </section>
+    );
 }
 
-export default JoinUs
+export default JoinUs;
